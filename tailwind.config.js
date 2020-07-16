51bcf72d2f284402e0d1b4ccf1d57e9ab1c86e3c
Alisson Fumaco <alisson3ds@hotmail.com>
2020-07-15 21:40:01 -0300

    Add tailwind config file

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  purge: [],
+  theme: {
+    extend: {},
+    inset: {
+      '0': '0',
+      'auto': 'auto',
+      '2/10': '20%'
+    },
+    zIndex: {
+      '-1': '-1'
+    },
+    fontFamily: {
+      'rubik': ['Rubik']
+    },
+    colors: {
+      'black-blue': 'hsl(229, 31%, 21%)',
+      'grayish-blue': 'hsl(229, 8%, 60%)',
+      'soft-blue': 'hsl(231, 69%, 60%)',
+      'soft-red': 'hsl(0, 94%, 66%)',
+      'white': '#fff'
+    },
+  },
+  variants: {},
+  plugins: [],
+}
